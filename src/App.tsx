b1e59581b20028af7f2bc7e34b817e80d5d24124
Kağan Değirmen <kagandegirmen@gmail.com>
2023-11-22 22:28:10 +0300

    Home > initialized, implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/scenes/navbar";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+import Home from "@/scenes/home";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -26,6 +27,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,81 @@
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+type Props = {
+  setSelectedPage: (page: SelectedPage) => void;
+};
+
+const Home = ({ setSelectedPage }: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* Image and Main Header */}
+      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
+        {/* Main Header */}
+        <div className="z-10 mt-32 md:basis-3/5">
+          {/* This means that on medium-sized screens and larger,
+           this element will take up 60% of its parent container's width. */}
+          {/* Headings */}
+          <div className="md:-mt-20">
+            <div className="relative">
+              <div className="before:absolute before:-left-20 before:-top-20 before:z-[-1] before:content-evolvetext md:before:content-evolvetext">
+                <img alt="home-page-text" src={HomePageText} />
+              </div>
+            </div>
+
+            <p className="mt-8 text-sm">
+              Our gym offers a wide range of fitness equipment and classes to
+              help you stay active and fit. With professional trainers and
+              state-of-the-art facilities, we provide a supportive and
+              motivating environment for all fitness levels. Whether you're a
+              beginner or an experienced athlete, our gym is designed to help
+              you achieve your fitness goals.
+            </p>
+          </div>
+          {/* Actions */}
+          <div className="mt-8 flex items-center gap-8">
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
+          </div>
+        </div>
+        {/* Image */}
+        <div
+          className="mt-16 flex basis-3/5 justify-center
+        md:z-10 md:ml-40 md:justify-items-end"
+        >
+          <img src={HomePageGraphic} alt="home-pageGraphic" />
+        </div>
+      </div>
+      {/* Sponsors */}
+      {isAboveMediumScreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img src={SponsorRedBull} alt="sponsor-redbull" />
+              <img src={SponsorForbes} alt="sponsor-forbes" />
+              <img src={SponsorFortune} alt="sponsor-fortune" />
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Home;
