59e2feace48626167c0e63005584e73910d6a8b4
Kağan Değirmen <kagandegirmen@gmail.com>
2023-11-26 15:22:02 +0300

    ClassData implemented

---
+++ src/scenes/classes/ClassData.tsx
@@ -0,0 +1,48 @@
+import { ClassType } from "@/shared/types";
+import image1 from "@/assets/image1.png";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
+
+const classes: Array<ClassType> = [
+  {
+    name: "Weight Training Classes",
+    description:
+      "Our weight training classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
+    image: image1,
+  },
+  {
+    name: "Yoga Classes",
+    description:
+      "Our yoga classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
+    image: image2,
+  },
+  {
+    name: "Ab Core Classes",
+    description:
+      "Our ab core classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
+    image: image3,
+  },
+  {
+    name: "Adventure Classes",
+    description:
+      "Our adventure classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
+    image: image4,
+  },
+  {
+    name: "Fitness Classes",
+    description:
+      "Our fitness classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
+    image: image5,
+  },
+  {
+    name: "Training Classes",
+    description:
+      "Our training classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
+    image: image6,
+  },
+];
+
+export default classes;
--- src/scenes/classes/index.tsx
+++ src/scenes/classes/index.tsx
@@ -1,54 +1,10 @@
 import { useContext } from "react";
 import { SelectedPage, ClassType } from "@/shared/types";
-import image1 from "@/assets/image1.png";
-import image2 from "@/assets/image2.png";
-import image3 from "@/assets/image3.png";
-import image4 from "@/assets/image4.png";
-import image5 from "@/assets/image5.png";
-import image6 from "@/assets/image6.png";
 import { motion } from "framer-motion";
 import HText from "@/shared/HText";
 import Class from "./Class";
 import PageContext from "@/shared/pageContext";
-
-const classes: Array<ClassType> = [
-  {
-    name: "Weight Training Classes",
-    description:
-      "Our weight training classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
-    image: image1,
-  },
-  {
-    name: "Yoga Classes",
-    description:
-      "Our yoga classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
-    image: image2,
-  },
-  {
-    name: "Ab Core Classes",
-    description:
-      "Our ab core classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
-    image: image3,
-  },
-  {
-    name: "Adventure Classes",
-    description:
-      "Our adventure classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
-    image: image4,
-  },
-  {
-    name: "Fitness Classes",
-    description:
-      "Our fitness classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
-    image: image5,
-  },
-  {
-    name: "Training Classes",
-    description:
-      "Our training classes are designed to help you achieve your fitness goals. We offer a variety of classes, including yoga, Pilates, and more.",
-    image: image6,
-  },
-];
+import classes from "./ClassData";
 
 type Props = {};
 
