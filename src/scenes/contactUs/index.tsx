7d8a81628eed5e90a5aba03f3fca01724a9a94bf
Kağan Değirmen <kagandegirmen@gmail.com>
2023-11-26 11:15:24 +0300

    contactUs > image addded

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-  const inputStyles = `mt-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
+  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
 
   const {
     register, // register an input
@@ -128,6 +128,27 @@ const ContactUs = ({ setSelectedPage }: Props) => {
               Submit
             </button>
           </form>
+        </motion.div>
+
+        {/* Image */}
+        <motion.div
+          className="relative mt-16 basis-2/5 md:mt-0"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+            <img
+              className="w-full"
+              src={ContactUsPageGraphic}
+              alt="contact-us-page-graphic"
+            />
+          </div>
         </motion.div>
       </div>
     </section>
